8b0b7061dfe917a3dea021643f1bc667f1ab29df
Sergio Prudencio <sergexpruve@gmail.com>
2023-08-23 17:26:00 -0400

    Terminando primer modulo del curso clases 1-10

---
+++ platzi-market/src/main/java/com/platzi/platzi/market/web/controller/HolaMundoController.java
@@ -0,0 +1,16 @@
+package com.platzi.platzi.market.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//ANOTACIONES RESTCONTROLLER Y REQUESTMAPPING
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController
+{
+    @GetMapping("/hola")
+    public String saludar()
+    {
+        return "Nunca Pares de aprender :D";
+    }
+}
