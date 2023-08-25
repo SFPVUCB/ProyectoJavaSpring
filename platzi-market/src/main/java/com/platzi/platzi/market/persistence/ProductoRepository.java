577c63555558e9ae5c68d63d89fe8e8133969fe6
Sergio Prudencio <sergexpruve@gmail.com>
2023-08-25 11:28:57 -0400

    Añadiendo @Repository

--- platzi-market/src/main/java/com/platzi/platzi/market/persistence/ProductoRepository.java
+++ platzi-market/src/main/java/com/platzi/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,13 @@ package com.platzi.platzi.market.persistence;
 
 import com.platzi.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+//@Repository con esta anotacion le indico a Spring que esta clase se encarga de interactuar con la DB.
+@Repository
+//@Component es una generalizacion de las anotaciones que indica que es un componente de spring
 public class ProductoRepository
 {
     private ProductoCrudRepository productoCrudRepository;
@@ -28,7 +31,17 @@ public class ProductoRepository
     {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
     }
-
-
+    public Optional<Producto> getProducto(int idProducto)
+    {
+        return productoCrudRepository.findById(idProducto);
+    }
+    public Producto save(Producto producto)
+    {
+        return productoCrudRepository.save(producto);
+    }
+    public void delete(int idProducto)
+    {
+         productoCrudRepository.deleteById(idProducto);
+    }
 
 }
